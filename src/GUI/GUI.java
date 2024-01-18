42eeea987fbe9ad74aa16d93b09ed3747c75759b
kaski <crosberg22@student.oulu.fi>
2024-01-19 00:53:30 +0200

    first steps to GUI

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -1,4 +1,71 @@
 package GUI;
+import javax.swing.*;
 
-public class GUI {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI implements ActionListener {
+    private JFrame frame;
+    private JPanel panel;
+    private JButton btn;
+    private JLabel sudokuCodeLabel;
+    private JTextField sudokuCode;
+    private JLabel sudokuSourceLabel;
+    private JTextField sudokuSource;
+    private JLabel success;
+
+
+    int count = 0;
+
+    public GUI() {
+        frame = new JFrame();
+        panel = new JPanel();
+
+        frame.setSize(350, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Solved sudoku tracker");
+        frame.add(panel);
+        panel.setLayout(null);
+
+
+        sudokuCodeLabel = new JLabel("Enter sudoku code");
+
+        sudokuCodeLabel.setBounds(10, 20, 125, 25);
+        panel.add(sudokuCodeLabel);
+
+        sudokuCode = new JTextField(20);
+        sudokuCode.setBounds(135, 20, 165, 25);
+        panel.add(sudokuCode);
+
+        sudokuSourceLabel = new JLabel("Enter sudoku source");
+        sudokuSourceLabel.setBounds(10, 50, 125, 25);
+        panel.add(sudokuSourceLabel);
+
+        sudokuSource = new JTextField(20);
+        sudokuSource.setBounds(135, 50, 165, 25);
+        panel.add(sudokuSource);
+
+        btn = new JButton("add");
+        btn.setBounds(10, 80, 80, 25);
+        btn.addActionListener(this);
+        panel.add(btn);
+
+        success = new JLabel("");
+        success.setBounds(10, 110, 300, 25);
+        panel.add(success);
+
+
+
+
+        //frame.pack();
+        frame.setVisible(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String code = sudokuCode.getText();
+        String source = sudokuSource.getText();
+        System.out.println(code + " " + source);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import SudokuObjects.*;
-
+import  GUI.*;
 public class Main {
     public static void main(String[] args) {
-        Sudoku asd = new Sudoku("700893001006000003004506000008300070000040000040005200000907500300000900500432006", 12, 12, 2002, true, "asd", Sudoku.Difficulty.DIABOLICAL);
-        asd.printBoard();
+       //Sudoku asd = new Sudoku("040190000090035070000060034600000000019000580000000003730040000050310060000087010", 12, 12, 2002, true, "asd", Sudoku.Difficulty.DIABOLICAL);
+       // asd.printBoard();
+        new GUI();
     }
 }
